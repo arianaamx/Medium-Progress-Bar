c78939736d98c093f059742a0bc90b355fd6a8fd
Ariana <ariana.milasincic@gmail.com>
2023-08-07 12:03:21 +0000

    Update 6 files

--- components/BasicProgressBarWithLabel/BasicProgressBarWithLabel.jsx
+++ components/BasicProgressBarWithLabel/BasicProgressBarWithLabel.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export const BasicProgressBarWithLabel = () => {
-    const currentValue = 30;
-    const maxValue = 100;
+  const currentValue = 30;
+  const maxValue = 100;
 
-    return (
-        <>
-            <label for="progress-bar">Uploading: </label>
-            <progress id="progress-bar" value={currentValue} max={maxValue}>{currentValue}%</progress>
-        </>
-    )
-}
+  return (
+    <>
+      <label htmlFor="progress-bar">Uploading: </label>
+      <progress id="progress-bar" value={currentValue} max={maxValue}>
+        {currentValue}%
+      </progress>
+    </>
+  );
+};
